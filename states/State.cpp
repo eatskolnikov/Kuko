6b8cebdc6adade669de1d4010792de02a6e6ed5b
Rejcx <racheljmorris@gmail.com>
2015-03-04 14:11:49 -0600

    Updated the State entity

--- states/State.cpp
+++ states/State.cpp
@@ -15,9 +15,7 @@ State::State( LuaManager* ptrLuaManager, const std::string& stateName, const std
     m_ptrLuaMgr = ptrLuaManager;
     m_stateName = stateName;
     m_ptrLuaMgr->LoadScript( scriptPath );
-    Logger::Out( "Load prereqs", "State::State" );
     m_ptrLuaMgr->State_LoadRequiredScripts();
-    Logger::Out( "Setup", "State::State" );
     m_ptrLuaMgr->State_Setup();
     Logger::Out( "end", "State::State" );
 }
