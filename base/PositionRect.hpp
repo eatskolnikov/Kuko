b6cc73a0d3b8fb6a6cf1782689b5bcaf8973e478
Rachel J Morris <racheljmorris@gmail.com>
2015-05-30 13:40:13 -0500

    Added constructors to the position rectangle

--- base/PositionRect.hpp
+++ base/PositionRect.hpp
@@ -8,6 +8,14 @@ class PositionRect
 {
     public:
     float x, y, w, h;
+    PositionRect() { ; }
+    PositionRect( float x, float y, float w, float h )
+    {
+        this->x = x;
+        this->y = y;
+        this->w = w;
+        this->h = h;
+    }
 };
 
 }
